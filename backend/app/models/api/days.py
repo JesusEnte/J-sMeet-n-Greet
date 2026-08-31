37c144a5dfc7f51686efae313d8423f28a1a435f
JesusEnte <bock-david@outlook.de>
2026-08-31 10:21:12 +0200

    feat: improve openapi docs for days api

--- backend/app/models/api/days.py
+++ backend/app/models/api/days.py
@@ -1,18 +1,33 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import Optional
 from typing_extensions import Annotated
 from datetime import date as Date
 import annotated_types
 
 class Base(BaseModel):
-    hours: bytes
+    hours: Annotated[bytes, annotated_types.Len(3, 3)] = Field(description="binary representation of a day's 24 hours, 1 = available, 0 = busy, formatted as hex values")
 
 class Update(BaseModel):
-    hours: Optional[Annotated[bytes, annotated_types.Len(3, 3)]]
+    hours: Optional[Annotated[bytes, annotated_types.Len(3, 3)]] = Field(description="binary representation of a day's 24 hours, 1 = available, 0 = busy, formatted as hex values")
 
-    model_config = {"val_json_bytes": "hex"}
+    model_config = {
+        "val_json_bytes": "hex",
+        "json_schema_extra": {
+            "examples": [{
+                "hours": "03fff0"
+            }]
+        }
+    }
 
 class Response(Base):
     date: Date
 
-    model_config = {"ser_json_bytes": "hex"}
+    model_config = {
+        "ser_json_bytes": "hex",
+        "json_schema_extra": {
+            "examples": [{
+                "hours": "0fff80",
+                "date": "2026-08-31"
+            }]
+        }
+    }
