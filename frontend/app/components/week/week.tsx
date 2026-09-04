aed159eebb68261739f096ac58d55f9c9ddd22ba
JesusEnte <bock-david@outlook.de>
2026-09-04 16:49:39 +0200

    fix: console warning

--- frontend/app/components/week/week.tsx
+++ frontend/app/components/week/week.tsx
@@ -1,5 +1,7 @@
 import Day from "~/components/day/day"
 
+import { dateToShortISO } from "~/utils/date"
+
 import './style.css'
 
 export default function Week({startDate}: {startDate: Date}){
@@ -7,7 +9,7 @@ export default function Week({startDate}: {startDate: Date}){
   for (let i = 0; i < 7; i++){
     let date = new Date(startDate)
     date.setDate(startDate.getDate()  + i)
-    dayComponents[i] = <Day date={date}/>
+    dayComponents[i] = <Day date={date} key={dateToShortISO(date)}/>
   }
   return <div className="week">
     {...dayComponents}
